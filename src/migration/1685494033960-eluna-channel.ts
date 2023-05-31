3d334ae51536b1647de92889a731ba2a264eeec4
Axel Cocat <ax.cocat@gmail.com>
2023-05-31 14:03:33 +0200

    fix: fix eluna migration

--- src/migration/1685494033960-eluna-channel.ts
+++ src/migration/1685494033960-eluna-channel.ts
@@ -4,16 +4,16 @@ export class elunaChannel1685494033960 implements MigrationInterface {
 	name = "elunaChannel1685494033960";
 
 	public async up(queryRunner: QueryRunner): Promise<void> {
-		await queryRunner.query(`ALTER TABLE "guild" RENAME TO "temporary_guild"`);
-		await queryRunner.query(`CREATE TABLE "guild" ("discord_id" varchar PRIMARY KEY NOT NULL, "misc_channel" varchar, "anticheat_reports_channel" varchar, "eluna_channel" varchar)`);
-		await queryRunner.query(`INSERT INTO "guild"("discord_id", "misc_channel", "anticheat_reports_channel") SELECT "discord_id", "misc_channel", "anticheat_reports_channel" FROM "temporary_guild"`);
-		await queryRunner.query(`DROP TABLE "temporary_guild"`);
+		await queryRunner.query(`CREATE TABLE "temporary_guild" ("discord_id" varchar PRIMARY KEY NOT NULL, "misc_channel" varchar, "anticheat_reports_channel" varchar, "eluna_channel" varchar)`);
+		await queryRunner.query(`INSERT INTO "temporary_guild" ("discord_id", "misc_channel", "anticheat_reports_channel") SELECT "discord_id", "misc_channel", "anticheat_reports_channel" FROM "guild"`);
+		await queryRunner.query(`DROP TABLE "guild"`);
+		await queryRunner.query(`ALTER TABLE "temporary_guild" RENAME TO "guild"`);
 	}
 
 	public async down(queryRunner: QueryRunner): Promise<void> {
-		await queryRunner.query(`CREATE TABLE "temporary_guild" ("discord_id" varchar PRIMARY KEY NOT NULL, "misc_channel" varchar, "anticheat_reports_channel" varchar)`);
-		await queryRunner.query(`INSERT INTO "temporary_guild"("discord_id", "misc_channel", "anticheat_reports_channel") SELECT "discord_id", "misc_channel", "anticheat_reports_channel" FROM "guild"`);
-		await queryRunner.query(`DROP TABLE "guild"`);
-		await queryRunner.query(`ALTER TABLE "temporary_guild" RENAME TO "guild"`);
+		await queryRunner.query(`ALTER TABLE "guild" RENAME TO "temporary_guild"`);
+		await queryRunner.query(`CREATE TABLE "guild" ("discord_id" varchar PRIMARY KEY NOT NULL, "misc_channel" varchar, "anticheat_reports_channel" varchar)`);
+		await queryRunner.query(`INSERT INTO "guild" ("discord_id", "misc_channel", "anticheat_reports_channel") SELECT "discord_id", "misc_channel", "anticheat_reports_channel" FROM "temporary_guild"`);
+		await queryRunner.query(`DROP TABLE "temporary_guild"`);
 	}
 }
